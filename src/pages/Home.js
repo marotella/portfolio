a47f8f73e357331a4cdb2265fafbed16fe7fc7ae
marotella <michelle.a.rotella@gmail.com>
2023-06-23 15:00:13 -0400

    tailwind installed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 
 function Home() {
     return (
-        <div>
-        <div><h2>About Me</h2>
-        <p>I am a full-stack software engineer, with a passion for using technology to make hard work easier.</p>
-        <img src={process.env.PUBLIC_URL + "/mrotella.jpeg"} alt="michelle" />
-        </div>
-        <div><h2>Projects</h2></div>
-        <div><h2>Contact</h2></div>
-        </div>
+        <main>
+            <section>
+                <div>
+                    <h2>About Me</h2>
+                    <p>I am a full-stack software engineer, with a passion for using technology to make hard work easier.</p>
+                </div>
+                <img src={process.env.PUBLIC_URL + "/mrotella.jpeg"} alt="michelle" />
+            </section>
+            <div><h2>Projects</h2></div>
+            <div><h2>Contact</h2></div>
+        </main>
     )
 
 }
